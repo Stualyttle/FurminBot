9a83e7b52d295a656f791d2deddddbdb8500081d
Stualyttle <you@example.com>
2026-05-03 13:18:13 +0200

    Update Node.js version and dependencies in Dockerfile and package.json

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Node.js runtime as a parent image
-FROM node:20.17.0
+FROM node:24.15.0
 
 # Set the working directory in the container
 WORKDIR /usr/src/bot
--- src/utils/get-jef-user.ts
+++ src/utils/get-jef-user.ts
@@ -1,7 +1,8 @@
+import { User } from 'discord.js';
 import client from '../main';
 import { JEF_ID } from '../../constants';
 
-export function getJefUser() {
+export function getJefUser(): User | undefined {
   const userId = JEF_ID;
   // get user from client
   const user = client.users.cache.get(userId);
